7c34eb67f636e881b58874734c6e6ef29de13289
Eden Ariel Guzman Andia <edevar7960@gmail.com>
2020-02-02 22:28:44 -0400

    i have created a demo of Factory pattern

---
+++ src/main/java/MainFactoryDemo.java
@@ -0,0 +1,13 @@
+public class MainFactoryDemo {
+    public static void main(String[]args){
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape circle = shapeFactory.getShape("CIRCLE");
+        circle.draw();
+
+        Shape rectangle = shapeFactory.getShape("RECTANGLE");
+        rectangle.draw();
+
+        Shape square = shapeFactory.getShape("SQUARE");
+        square.draw();
+    }
+}
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,5 @@
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Hey, I' am a Rectangle");
+    }
+}
